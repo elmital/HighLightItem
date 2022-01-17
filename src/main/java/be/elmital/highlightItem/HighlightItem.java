65ff1c18dc8008d67febdd935d971843b4d85ae1
elmital <54907162+elmital@users.noreply.github.com>
2022-01-17 18:20:53 +0100

    chore: add License (#1)
    * chore: add License
    
    * chore: add license headers

--- src/main/java/be/elmital/highlightItem/HighlightItem.java
+++ src/main/java/be/elmital/highlightItem/HighlightItem.java
@@ -1,3 +1,25 @@
+/*
+ *  This file is part of the HighLightItem distribution (https://github.com/elmital/HighLightItem).
+ *
+ *  HighLightItem minecraft mod
+ *  Copyright (C) 2022  elmital
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package be.elmital.highlightItem;
 
 import net.fabricmc.api.ModInitializer;
@@ -10,6 +32,15 @@ public class HighlightItem implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Mod init!");
+		LOGGER.info("""
+				-------------
+				 HighLightItem
+				 Copyright (C) 2022  elmital
+				 This program comes with ABSOLUTELY NO WARRANTY.
+				 This is free software, and you are welcome to redistribute it
+				 under certain conditions.
+				 See the GNU General Public License for more details. <https://www.gnu.org/licenses/>
+				-------------
+				""");
 	}
 }
