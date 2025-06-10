29962d3df0b358e5159d994e3dee31cdaac7e8c4
elmital <54907162+elmital@users.noreply.github.com>
2025-06-10 18:24:54 +0200

    chore&fix: better implementation for ConfigurationScreen that fix issues with resizing the game while screen is open

--- src/main/java/be/elmital/highlightItem/ConfigurationScreen.java
+++ src/main/java/be/elmital/highlightItem/ConfigurationScreen.java
@@ -27,7 +27,7 @@ import net.minecraft.client.gl.RenderPipelines;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.OptionListWidget;
+import net.minecraft.client.gui.widget.DirectionalLayoutWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.screen.ScreenTexts;
@@ -41,7 +41,6 @@ public class ConfigurationScreen extends GameOptionsScreen {
     int green;
     int blue;
     float alpha;
-    private OptionListWidget list;
 
     public ConfigurationScreen(GameOptions gameOptions) {
         super(null, gameOptions, Text.literal("HighLightItem"));
@@ -57,26 +56,41 @@ public class ConfigurationScreen extends GameOptionsScreen {
         HighlightItem.configurator.updateColor(new float[]{this.red / 255.0f, this.green / 255.0f, this.blue / 255.0f, this.alpha / 100.0f}, null);
     }
 
+    @Override
+    protected void initFooter() {
+        DirectionalLayoutWidget directionalLayoutWidget = this.layout.addFooter(DirectionalLayoutWidget.vertical()).spacing(8);
+        directionalLayoutWidget.getMainPositioner().alignHorizontalCenter();
+        DirectionalLayoutWidget directionalLayoutWidget2 = directionalLayoutWidget.add(DirectionalLayoutWidget.horizontal().spacing(8));
+        directionalLayoutWidget2.add(
+            ButtonWidget.builder(Text.translatable("options.highlightitem.color.vanilla"), (button -> {
+                this.red = (int) (Colors.HighLightColor.DEFAULT.getShaderColor()[0] * 255);
+                this.green = (int) (Colors.HighLightColor.DEFAULT.getShaderColor()[1] * 255);
+                this.blue = (int) (Colors.HighLightColor.DEFAULT.getShaderColor()[2] * 255);
+                this.alpha = Colors.HighLightColor.DEFAULT.getShaderColor()[3] * 100;
+                this.clearAndInit();
+            }
+            )).build()
+        );
+        directionalLayoutWidget2.add(ButtonWidget.builder(ScreenTexts.DONE, button -> close()).build());
+    }
 
     @Override
-    protected void init() {
-        this.list = new OptionListWidget(this.client, this.width, this);
-
-        this.list.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.red", SimpleOption.emptyTooltip(), (prefix, value) -> {
+    protected void addOptions() {
+        this.body.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.red", SimpleOption.emptyTooltip(), (prefix, value) -> {
             if (value < 0 || value > 255) {
                 return Text.literal("error");
             } else {
                 return GameOptions.getGenericValueText(prefix, value);
             }
         }, new SimpleOption.ValidatingIntSliderCallbacks(0, 255), this.red, (value) -> this.red = value));
-        this.list.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.green", SimpleOption.emptyTooltip(), (prefix, value) -> {
+        this.body.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.green", SimpleOption.emptyTooltip(), (prefix, value) -> {
             if (value < 0 || value > 255) {
                 return Text.literal("error");
             } else {
                 return GameOptions.getGenericValueText(prefix, value);
             }
         }, new SimpleOption.ValidatingIntSliderCallbacks(0, 255), this.green, (value) -> this.green = value));
-        this.list.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.blue", SimpleOption.emptyTooltip(), (prefix, value) -> {
+        this.body.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.blue", SimpleOption.emptyTooltip(), (prefix, value) -> {
             if (value < 0 || value > 255) {
                 return Text.literal("error");
             } else {
@@ -84,41 +98,20 @@ public class ConfigurationScreen extends GameOptionsScreen {
             }
         }, new SimpleOption.ValidatingIntSliderCallbacks(0, 255), this.blue, (value) -> this.blue = value));
 
-        this.list.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.alpha", SimpleOption.emptyTooltip(), (prefix, value) -> {
+        this.body.addSingleOptionEntry(new SimpleOption<>("options.highlightitem.color.alpha", SimpleOption.emptyTooltip(), (prefix, value) -> {
             if (value < 0 || value > 100) {
                 return Text.literal("error");
             } else {
                 return GameOptions.getGenericValueText(prefix, Text.of(value + "%"));
             }
         }, new SimpleOption.ValidatingIntSliderCallbacks(0, 100), (int) this.alpha, (value) -> this.alpha = (float) value));
-
-        this.addSelectableChild(this.list);
-        this.addDrawableChild(ButtonWidget.builder(Text.translatable("options.highlightitem.color.vanilla"), (button -> {
-            this.red = (int) (Colors.HighLightColor.DEFAULT.getShaderColor()[0] * 255);
-            this.green = (int) (Colors.HighLightColor.DEFAULT.getShaderColor()[1] * 255);
-            this.blue = (int) (Colors.HighLightColor.DEFAULT.getShaderColor()[2] * 255);
-            this.alpha = Colors.HighLightColor.DEFAULT.getShaderColor()[3] * 100;
-            this.clearAndInit();
-        })).dimensions(this.width / 2 - 200, this.height - 27, 175, 20).build());
-        this.addDrawableChild(ButtonWidget.builder(ScreenTexts.DONE, (button) -> close()).dimensions(this.width / 2 + 25, this.height - 27, 175, 20).build());
-    }
-
-    @Override
-    protected void addOptions() {
-        // NOTHING TO DO
     }
 
 
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         super.render(context, mouseX, mouseY, delta);
-        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 20, net.minecraft.util.Colors.WHITE);
-        this.list.render(context, mouseX, mouseY, delta);
         context.drawTexture(RenderPipelines.GUI_TEXTURED, Identifier.of("highlight_item", "textures/empty-color.png"), 5, 36 , 0, 0, (this.width / 2) - 164 , this.height - 72, 256, 256);
         context.drawBorder(4, 35 , (this.width / 2) - 163 , this.height - 70, ColorHelper.getArgb(255, 75, 75, 75));
         context.fill(RenderPipelines.GUI, 5, 36 , (this.width / 2) - 160 , this.height - 36, ColorHelper.getArgb((int) (this.alpha * 2.55F), this.red, this.green, this.blue));
-    }
-
-    public void renderBackground(DrawContext context, int mouseX, int mouseY, float delta) {
-        renderInGameBackground(context);
     }
 }
